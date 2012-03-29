fd3986ff329ecc34ae922210128bdb23ff47bee4
albanrousset@gmail.com <albanrousset@gmail.com@b3212809-2d9b-d5d5-baf0-8e92947f77b5>
2012-03-29 14:10:54 +0000

    Tounrment !

--- src/metaheuristics/AG_Simple_Main.java
+++ src/metaheuristics/AG_Simple_Main.java
@@ -2,6 +2,7 @@ package metaheuristics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import problems.*;
 
@@ -19,10 +20,22 @@ public class AG_Simple_Main {
 
 	public static void main(String[] args) {
 		Individual bestSolution = null;	
-
 		Problem problem = new Circle();
 		
-		AG_Simple algo = new AG_Simple(problem);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Taille population : ");
+		String str = sc.nextLine();
+		int taille_pop = Integer.parseInt(str);
+			System.out.println("Nombre de generations : ");
+			String str1 = sc.nextLine();
+			int nbrgeneration = Integer.parseInt(str);
+				System.out.println("Probabilite de croisement : ");
+				String str2 = sc.nextLine();
+				float tx_cross = Float.parseFloat(str);
+					System.out.println("Probabilite de mutation : ");
+					String str3 = sc.nextLine();
+					float tx_mutation = Float.parseFloat(str);
+		AG_Simple algo = new AG_Simple(problem, taille_pop, nbrgeneration, tx_mutation, tx_cross);
 		try {
 			bestSolution = algo.execute();
 		} catch (ClassNotFoundException e) {
